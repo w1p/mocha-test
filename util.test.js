67b7a45c7302b6d3aa0dafdec3ff0db5129a1dc3
William Peng <williampeng920@gmail.com>
2021-01-25 11:06:59 +0800

    Test with jest --watch

--- util.test.js
+++ util.test.js
@@ -1,8 +1,8 @@
 //import functions from our util.js
-const { TestScheduler } = require('jest');
+//const { TestScheduler } = require('jest');
 const { generateText } = require('./util');
 
-TestScheduler('Ouput name and age from generateText', () => {
+test('Ouput name and age from generateText', () => {
     //Actual testing code
     const fakeText = generateText('John', 12)
     expect(fakeText).toBe('John (12 years old)')
