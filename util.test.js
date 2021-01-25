62df8b46187c3d7aa8624d7573a3ac24e03ebd2c
William Peng <williampeng920@gmail.com>
2021-01-25 11:13:23 +0800

    Unit tests

--- util.test.js
+++ util.test.js
@@ -6,4 +6,15 @@ test('Ouput name and age from generateText', () => {
     //Actual testing code
     const fakeText = generateText('John', 12)
     expect(fakeText).toBe('John (12 years old)')
+    const fakeText2 = generateText('Aaron', 10)
+    expect(fakeText2).toBe('Aaron (10 years old)')
+
+});
+
+test('Test null value with generateText', () => {
+    //Actual testing code
+    const fakeText = generateText('', null)
+    expect(fakeText).toBe(' (null years old)')
+    const fakeText2 = generateText()
+    expect(fakeText2).toBe('undefined (undefined years old)')
 });
